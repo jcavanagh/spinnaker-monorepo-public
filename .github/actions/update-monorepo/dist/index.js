09926f4f5cba14d09808a739492b3c5577b8b977
Joe Cavanagh <jcavanagh@apple.com>
2024-10-03 13:49:13 -0700

    rebuild update-monorepo

--- .github/actions/update-monorepo/dist/index.js
+++ .github/actions/update-monorepo/dist/index.js
@@ -31285,7 +31285,7 @@ function generatePrBody(results) {
             // By default, the pull script merges `master`/`main`.  Release refs need an additional argument.
             const refArg = getLocalRef() == 'main' ? '' : `-r ${getLocalRef()} `;
             body += '\n```zsh\n';
-            body += `./scripts/pull.sh ${refArg}${result.repo}`;
+            body += `./pull.sh ${refArg}${result.repo}`;
             // The Git editor script will format the message with individual commits imported, so it's nice to read
             // Manual resolutions should use the output of that script as the commit message
             body +=
